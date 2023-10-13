6015fbe8a8650384e1638fd049019ac1d9ff5888
diegoAndrade777 <diego.rodrigo.andrade@gmail.com>
2023-10-13 10:04:03 -0300

    feat: send project

---
+++ src/main/java/br/com/diegoandrade/todolistjava/task/TaskController.java
@@ -0,0 +1,78 @@
+package br.com.diegoandrade.todolistjava.task;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.diegoandrade.todolistjava.utils.Utils;
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+  @Autowired
+  private ITaskRepository taskRepository;
+
+  @PostMapping("/")
+  public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+    var userId = request.getAttribute("userId");
+    taskModel.setUserId((UUID) userId);
+
+    var currentDate = LocalDateTime.now();
+    if (currentDate.isAfter(taskModel.getStartedAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .body("A data de início/data de término deve ser maior que a data atual");
+    }
+
+    if (taskModel.getStartedAt().isAfter(taskModel.getEndAt())) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .body("A data de início deve ser menor que a data de término");
+    }
+
+    var taskCreated = this.taskRepository.save(taskModel);
+    return ResponseEntity.status(HttpStatus.CREATED).body(taskCreated);
+  }
+
+  @GetMapping("/")
+  public List<TaskModel> list(HttpServletRequest request) {
+    var userId = request.getAttribute("userId");
+
+    var tasks = this.taskRepository.findByUserId((UUID) userId);
+
+    return tasks;
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
+    var task = this.taskRepository.findById(id).orElse(null);
+
+    if (task == null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .body("Tarefa não encontrada");
+    }
+
+    var userId = request.getAttribute("userId");
+
+    if (!task.getUserId().equals(userId)) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .body("Este usuário não tem permissão para alterar essa tarefa");
+    }
+
+    Utils.copyNonNullProperties(taskModel, task);
+
+    var taskUpdated = this.taskRepository.save(task);
+    return ResponseEntity.ok().body(this.taskRepository.save(taskUpdated));
+  }
+}
---
+++ src/test/java/br/com/diegoandrade/todolistjava/TodolistJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.diegoandrade.todolistjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
